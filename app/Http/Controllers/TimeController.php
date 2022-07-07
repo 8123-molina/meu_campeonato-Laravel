619d95ec318d0e4ce9bc204c4d4e5788fef59876
Dhyone Molina <molinaprojetos@gmail.com>
2022-07-06 21:48:24 -0300

    Criação de classe Time e funções  get e create

--- app/Http/Controllers/TimeController.php
+++ app/Http/Controllers/TimeController.php
@@ -3,8 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Time;
 
 class TimeController extends Controller
 {
-    //
+    /**
+    * Buscar dados especifico do banco de dados e retornar para a view
+    */
+    public function get($id){
+ 
+        $campeonato = Time::find($id);
+        return response()->json($campeonato);
+    }
+
+    public function create(Request $request){
+ 
+        $dados = new Time();
+        $dados->nome = $request->nome;
+        $dados->save();
+
+        return redirect('/');
+    }
 }
