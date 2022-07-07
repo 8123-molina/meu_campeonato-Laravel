cf70ecd3fa76e04494f43c2dc0d1883a230c6a21
Dhyone Molina <molinaprojetos@gmail.com>
2022-07-06 21:47:46 -0300

    Função create para inserir camponatos

--- app/Http/Controllers/CampeonatoController.php
+++ app/Http/Controllers/CampeonatoController.php
@@ -4,21 +4,37 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Campeonato;
+use App\Models\Time;
 
 class CampeonatoController extends Controller
 {
-   /**
+    /**
     * Buscar dados do banco de dados e retornar para a view
     */
     public function index(){
         $campeonatos = Campeonato::all();
+        $times = Time::all();
+
         return view('home',[
-            'campeonatos' => $campeonatos
+            'campeonatos' => $campeonatos,
+            'times' => $times
         ]);
     }
-
-    public function getCampeonato($id){
+    /**
+    * Buscar dados especifico do banco de dados e retornar para a view
+    */
+    public function get($id){
+ 
         $campeonato = Campeonato::find($id);
         return response()->json($campeonato);
+    }
+
+    public function create(Request $request){
+ 
+        $dados = new Campeonato();
+        $dados->campeonato = $request->campeonato;
+        $dados->save();
+
+        return redirect('/');
     }
 }
