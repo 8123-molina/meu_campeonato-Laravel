d9d3a14f1fb3b6468750aa1588a471d39910ace3
Dhyone Molina <molinaprojetos@gmail.com>
2022-07-06 22:30:01 -0300

    Criado rota deleteTime e sortearJogos

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,8 @@ use App\Http\Controllers\TimeController;
 Route::get('/', [CampeonatoController::class, 'index']);
 Route::post('/createCampeonato', [CampeonatoController::class, 'create'])->name('createCampeonato');
 Route::post('/createTime', [TimeController::class, 'create'])->name('createTime');
+Route::post('/deleteTime', [TimeController::class, 'delete'])->name('deleteTime');
 Route::post('/createPartida', [PartidaController::class, 'create'])->name('createPartida');
 Route::get('/time', [TimeController::class, 'getTime'])->name('getTime');
 Route::get('/partida', [PartidaController::class, 'getPartida'])->name('getPartida');
+Route::get('/sortearJogos', [PartidaController::class, 'sortear'])->name('sortearJgos');
