b8b2e62f137835979084e56bf62bde875d68d312
Dhyone Molina <molinaprojetos@gmail.com>
2022-07-07 21:47:14 -0300

    Função para sorteio dos jogos

--- app/Http/Controllers/PartidaController.php
+++ app/Http/Controllers/PartidaController.php
@@ -14,7 +14,7 @@ class PartidaController extends Controller
      */
     public function sortear($id = null){
 
-        //Traz dados do campeonato
+         //Traz dados do campeonato
             $campeonato = Campeonato::find($id);
 
         //Recebe os times do banco
@@ -28,9 +28,10 @@ class PartidaController extends Controller
 
         //Coleta os nomes dos times
             foreach ($slice as $nomeTime) {
-                $times[] = $nomeTime['nome'];
+                $nomeTimes[] = $nomeTime['nome'];
             }
-            $sorteioDuplas = array_chunk($times, 2);
+            
+            $sorteioDuplas = array_chunk($nomeTimes, 2);
 
         //Distribui os times nos jogos das quartas de final
             foreach ($sorteioDuplas as $time) {
@@ -38,11 +39,11 @@ class PartidaController extends Controller
                 $timeB = $time[1];
 
                 //chama o script Python que gera os gols
-                $gols = shell_exec("python3 c:/src/teste.py");
-                $gol  = explode("\n ", $gols, -1);
+                $gols = shell_exec("python c:/src/teste.py");
+                $gol  = explode("\n", $gols, -1);
 
                 //Jogos quartas de final
-                $jogoQuartasFinais[] = [
+                $jogosQuartasFinais[] = [
                     'timeA' => $timeA,
                     'timeB' => $timeB,
                     'golTimeA' => $gol[0],
@@ -50,7 +51,135 @@ class PartidaController extends Controller
                 ];
             }
 
+            //Verifica vencedores das quartas dde final
+            foreach ($jogosQuartasFinais as $quartasFinal) {
+                $timeAgol = $quartasFinal['golTimeA'];
+                $timeBgol = $quartasFinal['golTimeB'];
 
+                if ($timeAgol > $timeBgol) {
+                    $vencedoresQuartasFinais = $quartasFinal['timeA'];
+                } else {
+                    $vencedoresQuartasFinais = $quartasFinal['timeB'];
+                }
+
+                $semiFinais[] = $vencedoresQuartasFinais;
+            }
+            
+        //Distribui os times nos jogos das semi finais
+            for ($i=0; $i < 2; $i++) { 
+                
+                $sorteioDuplasSemiFinais = array_chunk($semiFinais, 2);
+
+                $timeA = $sorteioDuplasSemiFinais[$i][0];
+                $timeB = $sorteioDuplasSemiFinais[$i][1];
+
+                //Chama o script Python que gera os gols
+                $gols = shell_exec("python c:/src/teste.py");
+                $gol  = explode("\n", $gols, -1);
+
+                //Jogos semi finais
+                $jogosSemiFinais[] = [
+                    'timeA' => $timeA,
+                    'golTimeA' => $gol[0],
+                    'timeB' => $timeB,
+                    'golTimeB' => $gol[1]
+                ];
+
+                //Verifica vencedores das semi finais
+                foreach ($jogosSemiFinais as $semiFinal) {
+                    $timeAgol = $semiFinal['golTimeA'];
+                    $timeBgol = $semiFinal['golTimeB'];
+
+                    if ($timeAgol > $timeBgol) {
+                        $vencedoresSemiFinais = $semiFinal['timeA'];
+                        $viceVice = $semiFinal['timeB'];
+                    } else {
+                        $vencedoresSemiFinais = $semiFinal['timeB'];
+                        $viceVice = $semiFinal['timeA'];
+                    }
+                }
+
+                $terceiroLugar[] = $viceVice;
+                $final[] = $vencedoresSemiFinais;
+            }
+        //Jogo da Terceiro
+            $timeAterceiro = $terceiroLugar[0];
+            $timeBterceiro = $terceiroLugar[1];
+
+            //Chama o script Python
+            $gols= shell_exec("python c:/src/teste.py");
+            $gol = explode("\n", $gols, -1);
+
+            $jogoTerceiro[] = [
+                'timeA' => $timeAterceiro,
+                'golTimeA' => $gol[0],
+                'timeB' => $timeBterceiro,
+                'golTimeB' => $gol[1],
+            ];
+
+                
+            foreach ($jogoTerceiro as $terceiro ){
+                $timeAgolt = $terceiro['golTimeA'];
+                $timeBgolt = $terceiro['golTimeB'];
+                
+                //Define campeão
+                if($timeAgolt > $timeBgolt){
+                    $terceiroCampeonato = $terceiro['timeA'];
+                }else{
+                    $terceiroCampeonato = $terceiro['timeB'];
+                };
+            }
+            
+            //Vice campeão
+            $tCampeao[] = $terceiroCampeonato; 
+
+        //Jogo da final
+            $timeA = $final[0];
+            $timeB = $final[1];
+            //Chama o script Python
+            $gols= shell_exec("python c:/src/teste.py");
+            $gol = explode("\n", $gols, -1);
+
+            $jogoFinal[] = [
+                'timeA' => $timeA,
+                'golTimeA' => $gol[0],
+                'timeB' => $timeB,
+                'golTimeB' => $gol[1],
+            ];
+
+                
+            foreach ($jogoFinal as $final ){
+                $timeAgol = $final['golTimeA'];
+                $timeBgol = $final['golTimeB'];
+                
+                //Define campeão
+                if($timeAgol > $timeBgol){
+                    $vencedorCampeonato = $final['timeA'];
+                }else{
+                    $vencedorCampeonato = $final['timeB'];
+                };
+            }
+        
+        //Vice campeão
+        $viceCampeao = $vencedorCampeonato != $jogoFinal[0]['timeA'] ? $jogoFinal[0]['timeA'] : $jogoFinal[0]['timeB'];
+
+        $campeao[] = $vencedorCampeonato; 
+
+            //var_dump($nomeTimes);exit;
+
+        //Retornando o campeonato view com jogos do campeonato
+        return view('jogos', [ 
+            "campeonato"            => $campeonato,
+            "jogosQuartasFinais"    => $jogosQuartasFinais,
+            "jogosSemiFinais"       => $jogosSemiFinais,
+            "semiFinais"            => $semiFinais,
+            "jogosFinal"            => $jogoFinal,
+            "tCampeao"              => $tCampeao,
+            "viceCampeao"           => $viceCampeao,
+            "campeao"               => $campeao
+           
+            ] 
+        );
 
 
     }
