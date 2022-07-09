500ff60da5cd4b0753c1fa4e131aa88c6a601f0f
Dhyone <molinaprojetos@gmail.com>
2022-07-09 18:17:47 -0300

    Criação de rotas jogos/{id} e ajuste rota createPartida

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,16 @@ use App\Http\Controllers\CampeonatoController;
 use App\Http\Controllers\PartidaController;
 use App\Http\Controllers\TimeController;
 
+//Route Campeonato
 Route::get('/', [CampeonatoController::class, 'index']);
 Route::post('/createCampeonato', [CampeonatoController::class, 'create'])->name('createCampeonato');
+Route::get('/jogos/{id}', [CampeonatoController::class, 'getJogos'])->name('getJogos');
+
+//Route Time
 Route::post('/createTime', [TimeController::class, 'create'])->name('createTime');
 Route::post('/deleteTime', [TimeController::class, 'delete'])->name('deleteTime');
-Route::post('/createPartida', [PartidaController::class, 'create'])->name('createPartida');
 Route::get('/time', [TimeController::class, 'getTime'])->name('getTime');
-Route::get('/partida', [PartidaController::class, 'getPartida'])->name('getPartida');
+
+//Route Jogos
+Route::get('/createPartida/{id}', [PartidaController::class, 'createPartida'])->name('createPartida');
 Route::get('/sortearJogos/jogos/{id}', [PartidaController::class, 'sortear'])->name('sortearJgos');
