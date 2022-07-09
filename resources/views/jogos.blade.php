a3b9985f76b26da3373f7940813eb06338ae2c0e
Dhyone <molinaprojetos@gmail.com>
2022-07-09 18:18:11 -0300

    adequação de elmentos para recebimento de dados

--- resources/views/jogos.blade.php
+++ resources/views/jogos.blade.php
@@ -5,14 +5,14 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Meu Campeonato</title>
-        <link href="../../css/styles.css" rel="stylesheet">
-        <link href="../../js/bootstrap.css" rel="stylesheet">
+        <link href="../../public/css/styles.css" rel="stylesheet">
+        <link href="../../public/js/bootstrap.css" rel="stylesheet">
 
     </head>
     <body>
         <div class="container">
             <div class="column">
-                <div class="box-card row">
+                <div class="card-info border row">
                     <div class="card-info campeao column" style="width: 90%">
                         <h1>{{$campeonato->campeonato}}</h1>
                     </div>
@@ -20,6 +20,7 @@
             </div>
             </div>
         </div>
+        <a class="button left" href="{{'./home'}}">Voltar</a>
         <div class=" card-info wrap">
             <div class="card-info column" id="quartasFinais">
                 <div class="card-info info">
@@ -27,19 +28,20 @@
                 </div>
                 <div class="column">
                 <?php foreach ($jogosQuartasFinais as $quartasFinal):?>
-                    <div class="box-card row1" style="border-bottom: 1px solid #999999 ">
-                        <div class="card-info column">
-                            <div class="" name="<?= $quartasFinal['timeA'] ?>"><?= $quartasFinal['timeA'] ?></div>
-                            <div class="" name="<?= $quartasFinal['golTimeA'] ?>"><?= $quartasFinal['golTimeA'] ?></div>
-                        </div>
-                        <div class="card-info">
-                            <h2>vs</h2>
-                        </div>
-                        <div class="card-info column">
-                            <div class="" name="<?= $quartasFinal['timeB'] ?>"><?= $quartasFinal['timeB'] ?></div>
-                            <div class="" name="<?= $quartasFinal['golTimeB']?>"><?= $quartasFinal['golTimeB']?></div>
-                        </div>
+                    <div class="card-info row1" style="border-bottom: 1px solid #999999 ">
+                    <div class="card-info column">
+                        <div class="" name="<?= $quartasFinal->timeA ?>"><?= $quartasFinal->timeA ?></div>
+                        <div class="" name="<?= $quartasFinal->golTimeA ?>"><?= $quartasFinal->golTimeA ?></div>
                     </div>
+                    <div class="card-info">
+                        <h2>vs</h2>
+                    </div>
+                    <div class="card-info column">
+                        <div class="" name="<?= $quartasFinal->timeB ?>"> <?= $quartasFinal->timeB ?></div>
+                        <div class="" name="<?= $quartasFinal->golTimeB ?>"><?= $quartasFinal->golTimeB ?></div>
+                    </div>
+                </div>
+                    
                 <?php endforeach; ?>
                 </div>
             </div>
@@ -49,17 +51,17 @@
                 </div>
                 <div class="column">
                 <?php foreach ($jogosSemiFinais as $semiFinal):?>
-                    <div class="box-card row1" style="border-bottom: 1px solid #999999 ">
+                    <div class="card-info row1" style="border-bottom: 1px solid #999999 ">
                         <div class="card-info column">
-                            <div class="" name="<?= $semiFinal['timeA'] ?>"><?= $semiFinal['timeA'] ?></div>
-                            <div class="" name="<?= $semiFinal['golTimeA'] ?>"><?= $semiFinal['golTimeA'] ?></div>
+                            <div class="" name="<?= $semiFinal->timeA ?>"><?= $semiFinal->timeA ?></div>
+                            <div class="" name="<?= $semiFinal->golTimeA ?>"><?= $semiFinal->golTimeA ?></div>
                         </div>
                         <div class="card-info">
                             <h2>vs</h2>
                         </div>
                         <div class="card-info column">
-                            <div class="" name="<?= $semiFinal['timeB'] ?>"><?= $semiFinal['timeB'] ?></div>
-                            <div class="" name="<?= $semiFinal['golTimeB']?>"><?= $semiFinal['golTimeB']?></div>
+                            <div class="" name="<?= $semiFinal->timeB ?>"><?= $semiFinal->timeB ?></div>
+                            <div class="" name="<?= $semiFinal->golTimeB?>"><?= $semiFinal->golTimeB?></div>
                         </div>
                     </div>
                 <?php endforeach; ?>
@@ -71,17 +73,17 @@
                 </div>
                 <div class="column">
                 <?php foreach ($jogosFinal as $final):?>
-                    <div class="box-card row1" style="border-bottom: 1px solid #999999 ">
+                    <div class="card-info row1" style="border-bottom: 1px solid #999999 ">
                         <div class="card-info column">
-                            <div class="" name="<?= $final['timeA'] ?>"><?= $final['timeA'] ?></div>
-                            <div class="" name="<?= $final['golTimeA'] ?>"><?= $final['golTimeA'] ?></div>
+                            <div class="" name="<?= $final->timeA ?>"><?= $final->timeA ?></div>
+                            <div class="" name="<?= $final->golTimeA ?>"><?= $final->golTimeA ?></div>
                         </div>
                         <div class="card-info">
                             <h2>vs</h2>
                         </div>
                         <div class="card-info column">
-                            <div class="" name="<?= $final['timeB'] ?>"><?= $final['timeB'] ?></div>
-                            <div class="" name="<?= $final['golTimeB']?>"><?= $final['golTimeB']?></div>
+                            <div class="" name="<?= $final->timeB ?>"><?= $final->timeB ?></div>
+                            <div class="" name="<?= $final->golTimeB?>"><?= $final->golTimeB?></div>
                         </div>
                     </div>
                 <?php endforeach; ?>
@@ -103,7 +105,7 @@
                     <h2>Vice-Campeão</h2>
                 </div>
                 <div class="card-info">
-                    <h1><?= $viceCampeao ?></h1>
+                    <h1><?= $viceCampeao[0] ?></h1>
                 </div>
             </div>
 
@@ -116,11 +118,11 @@
                 </div>
             </div>
         </div>
+       
     </body>
-<script src={{ asset('./js/bootstrap-icons.svg') }} rel="styleshet"></script>
-<script src={{ asset('./js/jquery.js') }} rel="styleshet"></script>
-<script src={{ asset('./css/styles.css') }} rel="styleshet"></script>
-<script src={{ asset('./js/bootstrap.js') }} rel="styleshet"></script> 
+<script src="{{ asset('../js/jquery.js') }}" rel="styleshet"></script>
+<script src="{{ asset('css/styles.css') }}" rel="styleshet"></script>
+<script src="{{ asset('js/bootstrap.js') }}" rel="styleshet"></script> 
 <script type="text/javascript">
 </script>
 </html>
